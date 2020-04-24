e20f3ffb28e786c9e2d7e812a4aac0d3b28cfecf
Rene Robles <nastyzombie@gmail.com>
2020-04-24 10:04:17 -0500

    7 buttons added, mapped 1st button to key note and the rest are dependent to the actual scale value

---
+++ Functions.ino
@@ -0,0 +1,21 @@
+/*
+ * All of the calcultaions will e done here
+*/
+
+/*
+ * All of the calcultaions will e done here
+*/
+
+int getNextNote(char type,int keyNote ,int currentNoteIndex){
+  int offsetIndex;
+  int offset;
+  return 0;
+  }
+
+  int getNoteForButton (int buttonNumber){
+    switch(currentScaleType){
+      case 1: return KEY_NOTE+MAJOR[buttonNumber-1];
+      case 2: return KEY_NOTE+MINOR[buttonNumber-1];
+      case 3: return KEY_NOTE+DIMINISHED[buttonNumber-1];
+      }
+  }
